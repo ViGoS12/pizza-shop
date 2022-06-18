5216c7a578920edd4045b9947d1309697f65088f
Kirill <kirisakov4@yandex.ru>
2022-06-18 16:43:43 +0300

    Added Skeleton

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import Categories from './components/categories/Categories'
 import Sort from './components/sort/Sort'
 import PizzaBlock from './components/PizzaBlock'
 import { useEffect, useState } from 'react'
+import Skeleton from './components/PizzaBlock/Skeleton'
 
 function App() {
   const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     fetch('https://62adae8c402135c7acc4f3b7.mockapi.io/items')
       .then((res) => res.json())
-      .then((array) => setItems(array))
+      .then((array) => {
+        setItems(array)
+        setIsLoading(false)
+      })
   }, [])
 
   return (
@@ -25,9 +30,9 @@ function App() {
           </div>
           <h2 className='content__title'>Все пиццы</h2>
           <div className='content__items'>
-            {items.map((obj) => (
-              <PizzaBlock key={obj.id} {...obj} />
-            ))}
+            {isLoading
+              ? [...new Array(8)].map((_, i) => <Skeleton key={i} />)
+              : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
           </div>
         </div>
       </div>
---
+++ src/components/PizzaBlock/Skeleton.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ContentLoader from 'react-content-loader'
+
+export default function Skeleton() {
+  return (
+    <div>
+      <ContentLoader
+        className='pizza-block'
+        speed={2}
+        width={280}
+        height={531}
+        viewBox='0 0 280 531'
+        backgroundColor='#f3f3f3'
+        foregroundColor='#ecebeb'>
+        <circle cx='135' cy='122' r='122' />
+        <rect x='0' y='266' rx='15' ry='15' width='280' height='27.2' />
+        <rect x='0' y='313' rx='10' ry='10' width='280' height='88.4' />
+        <rect x='0' y='430' rx='15' ry='15' width='90' height='30' />
+        <rect x='126' y='420' rx='25' ry='25' width='152.26' height='46' />
+      </ContentLoader>
+    </div>
+  )
+}
