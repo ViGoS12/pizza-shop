24814c571be2e7b1aab0d2e8605af8aa19607dd3
Kirill <kirisakov4@yandex.ru>
2022-06-19 18:32:17 +0300

    Fixed logo import

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from '../../assets/img/pizza-logo.svg'
+import logo from '../../assets/svg/pizza-logo.svg'
 import { Link } from 'react-router-dom'
 import Search from '../Search/'
 
